9a8737ba8a7fbaea3acc70a293a95a6383806d49
Pedro Henrique Fernandes Lobo <lobophf@gmail.com>
2020-08-15 19:42:58 -0300

    feat: Add the multidimensional case for sub2ind
    The new sub2ind function is for cases of order two or higher. It
    calculates all the indices iteratively, from the highest to the lowest,
    until it reaches the first two indices. When this occurs, the function
    calls sub2ind2d again.
    
    - The "ArrayOfDimensions" is an input that holds the number of elements
    in the two dimensions of this array. The "arraySize" is an input that
    represents the number of dimensions of our array.
    - The "index" input represents the linear index to be converted.
    - And the "convertedIndex" is an array containing both subscripts.
    The first is the row, and the second is the column.
    
    See the demo/demo_3DArray.c file for more details.

--- aimt.h
+++ aimt.h
@@ -2,3 +2,18 @@ void ind2sub2D(const int *arrayOfDimensions, const int index, int convertedIndex
 	convertedIndex[1] = index % arrayOfDimensions[1];
 	convertedIndex[0] = index / arrayOfDimensions[1];
 }
+
+void ind2sub(const int *arrayOfDimensions, const int arraySize, const int index, int convertedIndex[]){  
+	int subscript = arraySize - 1;
+	if(subscript < 2){
+		ind2sub2D(arrayOfDimensions, index, convertedIndex);
+	}else{
+		int hypoDimensionsProduct = 1;
+		for(int i = 0; i < subscript; i++){
+			hypoDimensionsProduct *= arrayOfDimensions[i];
+		}
+		convertedIndex[subscript] = index / hypoDimensionsProduct;	
+		int newIndex = index % hypoDimensionsProduct;
+		ind2sub(arrayOfDimensions, subscript, newIndex, convertedIndex);
+	}
+}
