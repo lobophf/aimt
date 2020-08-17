6dc8ac805e576212b8c9665ede3f05fe8a9e300e
Pedro Henrique Fernandes Lobo <lobophf@gmail.com>
2020-08-17 09:27:51 -0300

    style: Changing names of files from demo folder
    In the next commits, I will add new features and create new demo files.
    So, to avoid messing up the names, I decided to change the names of the
    existing files.

---
+++ demo/demo_ind2sub.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "../aimt.h"
+
+/* 
+
+3x5x4 array sections filled with its 
+indexes in each respective position.
+
+dim3 = 0
+ 0  1  2  3  4
+ 5  6  7  8  9
+10 11 12 13 14
+
+dim3 = 1
+15 16 17 18 19
+20 21 22 23 24
+25 26 27 28 29
+
+dim3 = 2
+30 31 32 33 34
+35 36 37 38 39
+40 41 42 43 44
+
+dim3 = 3
+45 46 47 48 49
+50 51 52 53 54
+55 56 57 58 59
+
+*/
+
+int main(){
+
+	int arr[] = {3, 5, 4};
+	int index = 38;
+
+	int size = sizeof(arr)/sizeof(arr[0]);
+
+	int sub[size];
+	ind2sub(arr, size, index, sub);
+
+	printf("The linear index %d was converted"
+	       " to subscripts (%d, %d, %d) of a"
+	       " %dx%dx%d array.", index, sub[0], 
+	       sub[1], sub[2], arr[0], arr[1], 
+	       arr[2]);
+		       
+	return 0;
+}
