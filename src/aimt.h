9b0bc267fc06361b57f70e08354a9fb2cb055294
Pedro Henrique Fernandes Lobo <lobophf@gmail.com>
2020-09-03 21:49:11 -0300

    docs: Add comments to generate the documentation
    I created and edited the DoxygenConfig file to generate the
    documentation. Then I commented on the "aimt.h" file to add the needed
    comments.

--- src/aimt.h
+++ src/aimt.h
@@ -4,9 +4,100 @@
 #include "errorsHandler.h"
 #include "dimensionsProduct.h"
 
+/**
+ *
+ * @brief A special two dimensions case that converts subscripts into linear indices. 
+ *
+ * @param[in] arrayOfDimensions where each element represents the number of rows and 
+ * columns respectively.
+ * @param[in] arrayOfSubscripts in which the set of its elements form the position to be 
+ * converted.
+ * @return a converted linear index for two dimensions case.
+ *
+ * @pre Each element of @paramname{arrayOfSubscripts} must fit in the each element of the
+ * @paramname{arrayOfDimensions}.
+ * 
+ * @warning Ensure that the @paramname{arrayOfDimensions} and @paramname{arrayOfSubscripts} 
+ * have two dimensions, in order to get a proper result.
+ */
 int sub2ind2D(const int *arrayOfDimensions, const int *arrayOfSubscripts);
+
+/** 
+ *
+ * @example demo_sub2ind2D.c
+ * This is an example of how to use the sub2ind2D function.
+ */
+
+/**
+ *
+ * @brief Converts subscripts into linear indices. 
+ *
+ * @param[in] arrayOfDimensions where each element represents the size of each array 
+ * dimension. 
+ * @param[in] arrayOfSubscripts in which the set of its elements form the position to be 
+ * converted.
+ * @param[in] dimensions is size of the @paramname{arrayOfDimensions} and 
+ * @paramname{arrayOfSubscripts}.
+ * @return a converted linear index.
+ *
+ * @pre The parameter @paramname{dimensions} must be greater than or equal to 2. 
+ *
+ * @warning Ensure that @paramname{arrayOfDimensions} and @paramname{arrayOfSubscripts} 
+ * share the same @paramname{dimension}, in order to get a proper result.
+ */
 int sub2ind(const int *arrayOfDimensions, const int *arrayOfSubscripts, const int dimensions);
+
+/** 
+ *
+ * @example demo_sub2ind.c
+ * This is an example of how to use the sub2ind function.
+ */
+
+/**
+ *
+ * @brief A special two dimensions case that converts linear index into subscripts. 
+ *
+ * @param[in] arrayOfDimensions where each element represents the number of rows and 
+ * columns respectively.
+ * @param[in] index to be converted.
+ * @param[in, out] convertedIndex give the converted subscripts set.
+ * 
+ * @pre The @paramname{index} must fit in the @paramname{arrayOfDimensions}.
+ * @pre Each element of @paramname{arrayOfSubscripts} must fit in the each element of the
+ * @paramname{arrayOfDimensions}.
+ * 
+ * @warning Ensure that the @paramname{arrayOfDimensions} and @paramname{convertedIndex} 
+ * have two dimensions, in order to get a proper result.
+ */
 void ind2sub2D(const int *arrayOfDimensions, const int index, int convertedIndex[]);
+
+/** 
+ *
+ * @example demo_ind2sub2D.c
+ * This is an example of how to use the ind2sub2D function.
+ */
+
+/**
+ *
+ * @brief Converts linear index into subscripts. 
+ *
+ * @param[in] arrayOfDimensions where each element represents the number of rows and 
+ * columns respectively.
+ * @param[in] dimensions is size of the @paramname{arrayOfDimensions} and 
+ * @paramname{convertedIndex}.
+ * @param[in] index to be converted.
+ * @param[in, out] convertedIndex give the converted subscripts set.
+ * 
+ * @pre The @paramname{index} must fit in the @paramname{arrayOfDimensions}.
+ * 
+ * @warning Ensure that @paramname{arrayOfDimensions} and @paramname{arrayOfSubscripts} 
+ * share the same @paramname{dimension}, in order to get a proper result.
+ */
 void ind2sub(const int *arrayOfDimensions, const int dimensions, const int index, int convertedIndex[]);
 
+/** 
+ *
+ * @example demo_ind2sub.c
+ * This is an example of how to use the ind2sub function.
+ */
 #endif
